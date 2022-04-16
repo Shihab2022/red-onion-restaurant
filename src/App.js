a7d3043ea0c81c8e682eba29be50c0e98933221e
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-16 09:12:38 +0600

    handel some error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import BreakfastPage from './Pages/Home/BreakfastPage/BreakfastPage';
-import DinnerPage from './Pages/Home/DinnerPage/DinnerPage';
 import HomePage from './Pages/Home/HomePage/HomePage';
-import LunchPage from './Pages/Home/LunchPage/LunchPage';
 import SignIn from './Pages/Login/SignIn/SignIn';
 import NotFound from './Pages/NotFound/NotFound';
 import Header from './Pages/Shared/Header/Header';
@@ -17,9 +14,6 @@ function App() {
      <Routes>
     <Route path='/' element={<HomePage></HomePage>}></Route>
     <Route path='/home' element={<HomePage></HomePage>}></Route>
-    <Route path='/breakfast' element={<BreakfastPage></BreakfastPage>}></Route>
-<Route path='/lunch' element={<LunchPage></LunchPage>}></Route>
-<Route path='/dinner' element={<DinnerPage></DinnerPage>}></Route>
 <Route path='/signIn' element={<SignIn></SignIn>}></Route>
 <Route path='*' element={<NotFound></NotFound>}></Route>
 
--- src/Pages/Home/HomePage/HomePage.js
+++ src/Pages/Home/HomePage/HomePage.js
@@ -1,37 +1,40 @@
 import React, { useState } from "react";
 import useFoodData from "../../../Hook/foodData";
 import BannerPage from "../BannerPage/BannerPage";
-import BreakfastCard from "../BreakfastPage/BreakfastCard";
 import FoodCard from "../FoodCard/FoodCard";
 
 const HomePage = () => {
   const [givenData, setGivenData] = useState([]);
+  const [active,setActive] = useState(false)
   const [foodData] = useFoodData();
   const selectedFood = (response) => {
    
     const foodClass = foodData.filter((food) => food.id.includes(response));
-
     setGivenData(foodClass);
+    setActive(!active)
   };
  
-
+console.log(active);
   return (
     <div>
       <BannerPage></BannerPage>
 
       <div className="flex justify-center my-3 md:my-8 items-center text-2xl">
-        <button className="active"  onClick={() => selectedFood("breakfast")}>Breakfast</button>
-        <button className="mx-14" onClick={() => selectedFood("dinner")}>
+        <button className={`font-bold font-serif ${active ? 'text-rose-800 underline decoration-rose-800':''}`}  onClick={() => selectedFood("breakfast")}>Breakfast</button>
+        <button className={`font-bold font-serif mx-14 ${active ? 'text-rose-800 underline decoration-rose-800':''}`} onClick={() => selectedFood("dinner")}>
           Dinner
         </button>
-        <button onClick={() => selectedFood("lunch")}>Lunch</button>
+        <button className={`font-bold font-serif ${active ? 'text-rose-800 underline decoration-rose-800':''}`} onClick={() => selectedFood("lunch")}>Lunch</button>
       </div>
 
       <div className="md:px-28 my-10 grid md:grid-cols-3 grid-col-1 md:mb-0 mb-8 gap-5">
-        {givenData.map((data) => (
+      { givenData.length===0 ?foodData.map((data) => (
       
-          <FoodCard key={data.id} data={data}></FoodCard>
-        ))}
+      <FoodCard key={data.id} data={data}></FoodCard>
+    )) : givenData.map((data) => (
+      
+      <FoodCard key={data.id} data={data}></FoodCard>
+    )) }
         
       </div>
     </div>
