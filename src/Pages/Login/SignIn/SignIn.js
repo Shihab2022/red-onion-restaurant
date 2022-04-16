734c4fffb30860e0c55672cec5e3f7ed5969b11a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-16 14:38:11 +0600

    complete sign in page

--- src/Pages/Login/SignIn/SignIn.js
+++ src/Pages/Login/SignIn/SignIn.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import bannerbackground from "../../../images/bannerbackground.png";
 import logo2 from "../../../images/logo2.png";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
+import toast, { Toaster } from 'react-hot-toast';
+
 const SignIn = () => {
+  const [makeError,setMakeError]=useState('')
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth ,{sendEmailVerification:true});
+  const submitForm = (e) => {
+    e.preventDefault();
+    // const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+    if(password === confirmPassword){
+      createUserWithEmailAndPassword(email, password)
+    }
+    else{
+      setMakeError(' Password must be same  !!!!!!!!')
+    }
+  };
+
+  if(loading){
+    toast('Please wait......')
+  }
+
   return (
     <div className="relative mt-5">
       <img src={bannerbackground} className="h-screen" alt="" />
@@ -12,41 +37,56 @@ const SignIn = () => {
             <img style={{ height: "50px" }} src={logo2} alt="" />
           </div>
 
-          <form action="">
+          <form onSubmit={submitForm}>
             <div className="mt-4">
               <div>
-                <label className="block" for="Name">
-                  Name
-                </label>
+                <label className="block">Name</label>
                 <input
                   type="text"
                   placeholder="Name"
+                  name="name"
+                  required
                   className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                 />
               </div>
               <div className="mt-4">
-                <label className="block" for="email">
-                  Email
-                </label>
+                <label className="block">Email</label>
                 <input
                   type="text"
                   placeholder="Email"
+                  name="email"
+                  required
                   className="w-full px-4 py-2  mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                />
+              </div>
+              {/* <p>{loading && 'loading....'}</p> */}
+              <div className="mt-4">
+                <label className="block"> Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  required
+                  className="w-full px-4 py-2 mt-2 border  rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                 />
               </div>
               <div className="mt-4">
                 <label className="block">Confirm Password</label>
                 <input
                   type="password"
-                  placeholder="Password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  required
                   className="w-full px-4 py-2 mt-2 border  rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                 />
               </div>
               <span className="text-xs text-red-400">
-                Password must be same!
+                {
+                  makeError ? makeError:''
+                }
               </span>
               <div className="flex">
-                <button className="w-full px-6 py-3 mt-4 text-white bg-rose-700 rounded-lg hover:bg-blue-900">
+                <button className="w-full px-6 py-3 mt-4 text-white bg-rose-700 rounded-lg hover:bg-blue-500">
                   Sign In
                 </button>
               </div>
@@ -55,9 +95,11 @@ const SignIn = () => {
                 <button className="text-blue-600 hover:underline" href="#">
                   Login
                 </button>
+                <Toaster />
               </div>
             </div>
           </form>
+
         </div>
       </div>
     </div>
